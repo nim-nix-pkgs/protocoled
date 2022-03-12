7fd6b36141abe56eada7553326dfbe23fb637736
hugosenari <hugosenari@gmail.com>
2022-03-12 04:06:47 -0300

    chore: re index protocoled

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''an interface macro'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-protocoled-master.flake = false;
+  inputs.src-protocoled-master.owner = "planetis-m";
+  inputs.src-protocoled-master.ref   = "refs/heads/master";
+  inputs.src-protocoled-master.repo  = "protocoled";
+  inputs.src-protocoled-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-protocoled-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-protocoled-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
